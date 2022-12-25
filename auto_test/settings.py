2710cc61a21986edce45c54e9b05f186e7e37465
Sok-Solutions <116672004+Sok-Solutions@users.noreply.github.com>
2022-12-25 19:10:32 +0100

    Update settings.py

--- auto_test/settings.py
+++ auto_test/settings.py
@@ -26,7 +26,7 @@ HASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS=['https://walrus-app-8urx2.ondigitalocean.app/']
